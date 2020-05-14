8a1652349cc3a022239f45ccef5062d01dd98acd
Matthew Cohen <matt@mattbsg.xyz>
2020-05-14 14:45:52 -0400

    401

--- core/utils.py
+++ core/utils.py
@@ -105,6 +105,11 @@ def authrequired():
 
             whitelist = config.get("oauth_whitelist", [])
             if document:
+                if str(app.bot_id) != document.get("bot_id"):
+                    abort(
+                        401,
+                        message="Your account does not have permission to view this page.",
+                    )
                 whitelist.extend(document.get("oauth_whitelist", []))
 
             if int(user["id"]) in whitelist or "everyone" in whitelist:
